3a945f6c15bcca7aa35afce0640b116a3f342bfc
aferguson9806 <aferguson9806@gmail.com>
2021-10-03 14:54:04 -0400

    form and post request, footer on page

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 
-app.route('/api')
+/* app.route('/api')
   .get(async (req, res) => {
     console.log('GET request detected');
     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
@@ -39,6 +39,16 @@ app.route('/api')
     const json = await data.json();
 
     res.json({ data: json })
+  }); */
+
+app.route('/results')
+  .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+    console.log('Now send something back to your client');
+
+
+    res.send({ data: req.body })
   });
 
 app.listen(port, () => {
