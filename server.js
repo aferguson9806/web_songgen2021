3aaf476a321d60d9e3a40692d896e7b743bca3a4
aferguson9806 <aferguson9806@gmail.com>
2021-10-18 11:58:26 -0400

    import to require

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
-import PythonShell from 'python-shell';
+let {PythonShell} = require('python-shell'); 
 
 dotenv.config();
 
