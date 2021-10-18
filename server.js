89dd3b1b36d204599154ce6b8ede1aafcfa4ed50
aferguson9806 <aferguson9806@gmail.com>
2021-10-18 11:53:05 -0400

    changing name of import

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
-import {PythonShell} from 'python-shell';
+import PythonShell from 'python-shell';
 
 dotenv.config();
 
